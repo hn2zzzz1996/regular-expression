30b080c6afcae22f3bd397a171cf6b404e0d2de9
hn2zzzz1996 <huanggua5678@qq.com>
2019-11-23 16:59:30 +0800

    nfa

--- README.md
+++ README.md
@@ -15,12 +15,42 @@
 使用多个**State**结构体的链接表示NFA：
 
 ```C
+/*
+ * Represents an NFA state plus zero or one or two arrows exiting.
+ * if c == 256, no arrows out; matching state.
+ * If c == 257, unlabeled arrows to out and out1 (if != NULL).
+ * If c < 256, labeled arrow with character c to out.
+ */
 struct State
 {
 	int c;
 	State *out;
 	State *out1;
 	int lastlist;
+};
+```
+
+> 一个State最多只有两个分支。每个State都代表着一个NFA的片段。
+
+对NFA片段的描述采用Frag结构体：
+
+```C
+struct Frag
+{
+	State *start;
+	Ptrlist *out;
+};
+```
+
+> start表示开始状态；out表示这个NFA片段中未连接到其他State的悬挂指针，是一系列的**State***的指针
+
+
+
+```C
+union Ptrlist
+{
+	Ptrlist *next;
+	State *s;
 };
 ```
 
--- reg.c
+++ reg.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 char *reg2post(char *reg){
 	char *p = reg;
@@ -102,6 +103,168 @@ char *reg2post(char *reg){
 	*q = '\0';
 	//printf("%s\n", res);
 	return res;
+}
+
+/*
+ * Represents an NFA state plus zero or one or two arrows exiting.
+ * if c == Match, no arrows out; matching state.
+ * If c == Split, unlabeled arrows to out and out1 (if != NULL).
+ * If c < 256, labeled arrow with character c to out.
+ */
+enum{
+	Match = 256,
+	Split = 257
+};
+typedef struct State State;
+struct State{
+	int c;
+	State *out;
+	State *out1;
+	int lastlist;
+};
+State matchstate = {Match};
+int nstate;
+
+State *state(int c, State *out, State *out1){
+	State *s;
+	
+	nstate++;
+	s = malloc(sizeof *s);
+	s->c = c;
+	s->out = out;
+	s->out1 = out1;
+	s->lastlist = 0;
+	return s;
+}
+
+typedef union Ptrlist Ptrlist;
+union Ptrlist{
+	Ptrlist *next;
+	State *s;
+};
+
+/*
+ * 要是不把Ptrlist定义成union的话，就应该这么写
+ * struct Ptrlist{
+ * 	 Ptrlist *next;
+ *   State **s;
+ * } 
+ */
+
+Ptrlist *list1(State **outp){
+	/* 重复利用指针所在的地址空间，指针未赋值时就是挂在Ptrlist上的 */
+	Ptrlist *l;
+	
+	l = (Ptrlist *)outp;
+	l->next = NULL;
+	return l;
+}
+
+/* Patch the list of states at out to point to start. */
+Ptrlist *patch(Ptrlist *l, State *s){
+	Ptrlist *next;
+	
+	for(; l; l=next){
+		next = l->next;
+		l->s = s;
+	}
+}
+
+Ptrlist *append(Ptrlist *l1, Ptrlist *l2){
+	Ptrlist *oldl1;
+	
+	oldl1 = l1;
+	while(l1->next){
+		l1 = l1->next;
+	}
+	l1->next = l2;
+	return oldl1;
+}
+
+typedef struct Frag Frag;
+struct Frag{
+	State *start;
+	Ptrlist *out;
+};
+Frag frag(State *start, Ptrlist *out){
+	Frag n = { start, out };
+	return n;
+}
+
+State *post2nfa(char *postfix){
+	char *p;
+	Frag stack[1000], *stackp, e1, e2, e;
+	State *s;
+	Ptrlist *l;
+	
+	#define push(s) *stackp++ = s
+	#define pop()   *--stackp
+	
+	stackp = stack;
+	for(p = postfix; *p; p++){
+		switch(*p){
+			case '.':
+				e2 = pop();
+				e1 = pop();
+				patch(e1.out, e2.start);
+				push(frag(e1.start, e2.out));
+				break;
+			case '|':
+				e2 = pop();
+				e1 = pop();
+				s = state(Split, e1.start, e2.start);
+				push(frag(s, append(e1.out, e2.out)));
+				break;
+			case '+':
+				e = pop();
+				s = state(Split, e.start, NULL);
+				patch(e.out, s);
+				push(frag(e.start, list1(&s->out1)));
+			case '*':
+				e = pop();
+				s = state(Split, e.start, NULL);
+				patch(e.out, s);
+				push(frag(s, list1(&s->out1)));
+				break;
+			case '?':
+				e = pop();
+				s = state(Split, e.start, NULL);
+				push(frag(s, list1(&s->out1)));
+				break;
+			default:
+				s = state(*p, NULL, NULL);
+				push(frag(s, list1(&s->out)));
+				break;
+		}
+	}
+	
+	e = pop();
+	patch(e.out, &matchstate);
+	return e.start;
+#undef pop
+#undef push
+}
+
+typedef struct List List;
+struct List{
+	State **s;
+	int n;
+};
+List l1, l2;
+static int listid;
+
+List *startList(State *start, List *l){
+	l->n = 0;
+	listid++;
+	addstate(l, start);
+	return l;
+}
+
+void addstate(List *l, State *s){
+	if(s == NULL || s->lastlist == listid)
+		return;
+		
+	l->s[l->n++] = s;
 }
 
 int main(){
